68be3e61ad8260c8a439dc5b0342d1fce13be397
0ri0nexe <enzo.mongin10@gmail.com>
2023-11-01 12:45:23 +0100

    fixed permissions

--- src-tauri/src/file_handle/permission.cpp
+++ src-tauri/src/file_handle/permission.cpp
@@ -8,28 +8,35 @@ extern "C"
     {
         std::cout << "call" << std::endl;
         std::string path(reinterpret_cast<const char *>(pathBytes), lenght);
-        std::filesystem::directory_entry entry(path);
         std::cout << "received " << path << std::endl;
-
-        if (entry.exists())
+        try
         {
-            if (entry.is_regular_file())
-            {
-                std::cout << "regular_file" << std::endl;
-                return 0;
-            }
-            else if (entry.is_directory())
-            {
-                std::cout << "regular_directory" << std::endl;
+            std::filesystem::directory_entry entry(path);
+            std::cout << "file getter OK" << path << std::endl;
 
-                return 0;
+            if (entry.exists())
+            {
+                if (entry.is_regular_file())
+                {
+                    std::cout << "regular_file" << std::endl;
+                    return 0;
+                }
+                else if (entry.is_directory())
+                {
+                    std::cout << "regular_directory" << std::endl;
+
+                    return 0;
+                }
+            }
+            else
+            {
+                return 1;
             }
         }
-        else
+        catch (...)
         {
+            std::cout << "Error occured " << path << std::endl;
             return 1;
         }
-
-        return 1;
     }
 }
